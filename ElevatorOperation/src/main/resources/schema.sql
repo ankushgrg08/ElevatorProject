d32fc593e5c5bfd266ac95a6880cf474e04b682d
ankushgrg08 <ankushgrg08@gmail.com>
2022-09-22 14:11:45 +0530

    ElevatorOperation Project

---
+++ ElevatorOperation/src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+DROP TABLE IF EXISTS elevator1;  
+CREATE TABLE elevator1 (   
+elevator_name VARCHAR(50) NOT NULL,  
+floor_movement INT(8) NOT NULL
+CURRENT_DATE dateTime NOT NULL
+);
---
+++ ElevatorOperation/src/test/java/com/elevator/operator/ElevatorOperationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elevator.operator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElevatorOperationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
